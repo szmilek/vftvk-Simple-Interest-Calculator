51f1622c94602d647db9cc8ff4e057044de25690
szmilek <sabinaaklos@gmail.com>
2022-02-07 23:24:05 +0100

    Calculator - finished

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+
+    let interest = principal * years * rate / 100;
+    let endYear = new Date().getFullYear() + parseInt(years);
+
+
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    document.getElementById("result").innerHTML = `<br>If you deposit is <mark>${principal}</mark>,<br>at an interest rate of 
+    <mark>${rate}%</mark><br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br> <br> `;
 }
-        
+
+
+function updateRate()
+{
+    let updatedRate = document.getElementById("rate").value;
+    document.getElementById("updatedRate").innerText = `${updatedRate}%`;
+}
